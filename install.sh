fc8ba0846cc904b5be87a28fa4784fb02a864538
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2024-01-09 21:06:50 +0330

    update installer

---
+++ install.sh
@@ -0,0 +1,49 @@
+#/bin/bash 
+
+sleep 1 
+clear 
+echo "started ... "
+sleep 1 
+clear 
+
+apt update && apt install  lolcat  && apt install figlet >> /dev/null
+clear 
+
+figlet github OuTiS92 | lolcat -t -s -d 
+
+
+sleep 3 
+apt install influxdb influxdb-client >> /dev/null
+
+systemctl start influxdb 
+
+if systemctl is-active --quiet fail2ban ; then
+	echo "service infulxdb is running ..."
+	echo "I was getting port alrady in use error ... " 
+	sleep 2 
+	clear 
+
+
+else 
+	echo " service influxdb not runnint !!!!!!"
+	break
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

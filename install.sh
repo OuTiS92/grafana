dcaa97306614de1f310a33d1bf6e2d415caf9276
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2024-02-08 20:21:54 +0330

    update grafana

--- install.sh
+++ install.sh
@@ -189,7 +189,7 @@ sudo systemctl start mysql_exporter
 
 install_yamel_prometheus() {
 cd ./prometheus-2.45.3.linux-amd64
-	cat <<EOF >> prometheus.yml 
+	cat <<EOF >> ./prometheus-2.45.3.linux-amd64/prometheus.yml 
 - job_name: "mysqld"
 
     # metrics_path defaults to '/metrics'
